7fdc0f49a676097a167c7fef6a6f845cd303d004
TCDorman <timothycolindorman@gmail.com>
2021-12-03 11:53:54 -0500

    Final

--- password.js
+++ password.js
@@ -1,2 +1,12 @@
 console.log(`Welcome to the password validator tool!`)
-var password = prompt(`what is your password?`)
+reader.question(`what is your password?`, function($answer))
+    
+}
+//for(let i = 0; i <= 10; i++){
+    //if(`$answer < 10){
+//         console.log(failure){
+//         } else {
+//             console.log(success)
+//         }
+//     }
+// }
